dc3f9062822c69f1aa3abbeb22065b0d988ce69b
facuaragon <facundo_aragon@hotmail.com>
2023-09-27 20:21:09 -0300

    images uploading to cloudinary

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,14 +7,18 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
-  images,
+  images: existingImages,
 }) {
   const router = useRouter();
+
+  const [imageSrc, setImageSrc] = useState([]);
+  const [uploadData, setUploadData] = useState();
 
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
   const [goToProducts, setGoToProducts] = useState(false);
+  const [images, setImages] = useState(existingImages || []);
   //   console.log(_id);
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -31,19 +35,45 @@ export default function ProductForm({
   if (goToProducts) {
     router.push("/products");
   }
-  const uploadImages = async (e) => {
-    if (e.target?.files?.length > 0) {
-      const formData = new FormData();
-      for (const file of e.target.files) {
-        formData.append("file", file);
-      }
-      const res = await fetch("/api/upload", {
-        method: "POST",
-        body: formData,
+
+  const uploadClodinaryImages = async (e) => {
+    const files = e.target?.files;
+    if (files?.length > 0) {
+      const promises = Array.from(files).map((file) => {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+
+          reader.onloadend = () => {
+            resolve(reader.result);
+          };
+
+          reader.onerror = (error) => {
+            reject(error);
+          };
+
+          reader.readAsDataURL(file);
+        });
       });
-      const data = await res.json();
-      console.log(data);
+
+      try {
+        const results = await Promise.all(promises);
+        const res = await axios.post("/api/upload", results);
+        console.log(res.data);
+        // setImageSrc([...imageSrc, ...results]);
+        // console.log([...imageSrc, ...results]);
+        setImages([...images, ...res.data]);
+      } catch (error) {
+        console.error(error);
+      }
     }
+  };
+
+  const deleteImage = async (e) => {
+    // console.log(e.target.id);
+    const array = [...images];
+    const positionToRemove = Number(e.target.id);
+    array.splice(positionToRemove, 1);
+    setImages([...array]);
   };
 
   return (
@@ -58,7 +88,7 @@ export default function ProductForm({
           onChange={(e) => setTitle(e.target.value)}
         ></input>
         <label>Photos:</label>
-        <div className="mb-2">
+        <div className="mb-2 flex gap-3 flex-wrap">
           <label className="cursor-pointer w-24 h-24 flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -71,18 +101,59 @@ export default function ProductForm({
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            <div>Upload</div>
+
+            <div>Add</div>
             <input
               type="file"
-              multiple="multiple"
+              name="file"
+              multiple
               accept="image/png, image/jpeg"
               className="hidden"
-              onChange={uploadImages}
+              onChange={uploadClodinaryImages}
             ></input>
           </label>
+          {images &&
+            images.map((image, i) => (
+              <div
+                key={i}
+                className="relative border rounded-md overflow-hidden"
+              >
+                <img
+                  src={image}
+                  width={100}
+                  style={{ maxHeight: "100px", width: "auto" }}
+                  alt="Image"
+                />
+                <button
+                  name={i}
+                  type="button"
+                  onClick={deleteImage}
+                  className="absolute top-1 right-1 bg-gray-200 text-gray-500 rounded-xl"
+                >
+                  <div
+                    id={i}
+                    className="absolute w-full h-full z-1 bg-transparent"
+                  ></div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 z-0"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            ))}
           {!images?.length && <div>No photos in this product</div>}
         </div>
         <label>Product Description</label>
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,36 +1,33 @@
-// import { v2 as cloudinary } from "cloudinary";
-// // Configure Cloudinary with your credentials
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-import { Readable } from "stream";
-import fs from "fs";
-import path from "path";
-import { promisify } from "util";
-
-const pipeline = promisify(require("stream").pipeline);
+import { v2 as cloudinary } from "cloudinary";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const uploadDir = path.join(process.cwd(), "uploads"); // Set your upload destination here.
-    const fileName = "your-file-name.ext"; // Set the desired file name.
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
-    const fileStream = Readable.from(req);
-    const writeStream = fs.createWriteStream(path.join(uploadDir, fileName));
+  const files = req.body;
+  const uploadResults = [];
+  const cloudinaryLinks = [];
 
-    fileStream.pipe(writeStream);
-
-    writeStream.on("error", (err) => {
-      console.error(err);
-      res.status(500).json({ message: "Error uploading the file" });
-    });
-
-    writeStream.on("finish", () => {
-      res.json({ message: "Image uploaded" });
-    });
-  } else {
-    res.status(405).end(); // Method not allowed
+  for (const file of files) {
+    try {
+      const cloudinaryResult = await cloudinary.uploader.upload(file, {
+        folder: "Ecommerce",
+      });
+      if (!cloudinaryResult) {
+        throw new Error("Error en la carga del archivo a Cloudinary");
+      }
+      cloudinaryLinks.push(cloudinaryResult.secure_url);
+      uploadResults.push(cloudinaryResult);
+    } catch (error) {
+      console.error("Error:", error);
+      res
+        .status(500)
+        .json({ error: "Error en la carga del archivo a Cloudinary" });
+      return;
+    }
   }
+  res.json(cloudinaryLinks);
 }
